789407ddb621eeec1dc06faaa08cc96143ed8538
jeyofdev <jgregoire.pro@gmail.com>
2020-05-03 15:36:59 +0200

    create the taxonomy for post type 'property'

---
+++ web/app/themes/estateagency/archive-property.php
@@ -0,0 +1,60 @@
+<?php get_header(); ?>
+
+
+<!-- Property Section Begin -->
+<section class="property-section spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-9">
+                <h4 class="property-title">Property</h4>
+                <div class="property-list">
+                    <div class="single-property-item">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="property-pic">
+                                    <img src="img/properties/property-1.jpg" alt="">
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="property-text">
+                                    <div class="s-text">For Sale</div>
+                                    <h5 class="r-title">Villa 9721 Glen Creek</h5>
+                                    <div class="room-price">
+                                        <span>Start From:</span>
+                                        <h5>$3.000.000</h5>
+                                    </div>
+                                    <div class="properties-location"><i class="icon_pin"></i> 9721 Glen Creek Ave.
+                                        Ballston Spa, NY</div>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                                        incididunt ut labore.</p>
+                                    <ul class="room-features">
+                                        <li>
+                                            <i class="fa fa-arrows"></i>
+                                            <p>5201 sqft</p>
+                                        </li>
+                                        <li>
+                                            <i class="fa fa-bed"></i>
+                                            <p>8 Bed Room</p>
+                                        </li>
+                                        <li>
+                                            <i class="fa fa-bath"></i>
+                                            <p>7 Baths Bed</p>
+                                        </li>
+                                        <li>
+                                            <i class="fa fa-car"></i>
+                                            <p>1 Garage</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Property Section End -->
+
+
+<?php get_footer(); ?>
--- web/app/themes/estateagency/inc/post-types.php
+++ web/app/themes/estateagency/inc/post-types.php
@@ -41,6 +41,84 @@ function estateagency_add_post_type () : void
         "taxonomies" => [],
         "has_archive" => true,
     ]);
+
+
+    register_taxonomy("property_contract_type", "property", [
+        "labels" => [
+            "name"                       => __( "Contract types", "estateagency"),
+            "singular_name"              => __( "Contract type", "estateagency"),
+            "search_items"               => __( "Search Contract types", "estateagency"),
+            "popular_items"              => __( "Popular Contract types", "estateagency"),
+            "all_items"                  => __( "All Contract types", "estateagency"),
+            "edit_item"                  => __( "Edit Contract type", "estateagency"),
+            "view_item"                  => __( "View Contract type", "estateagency"),
+            "update_item"                => __( "Update Contract type", "estateagency"),
+            "add_new_item"               => __( "Add New Contract type", "estateagency"), 
+            "new_item_name"              => __( "New Contract type Name", "estateagency"),
+            "separate_items_with_commas" => __( "Separate Contract types with commas", "estateagency"),
+            "add_or_remove_items"        => __( "Add or remove Contract types", "estateagency"),
+            "choose_from_most_used"      => __( "Choose from the most used Contract types", "estateagency"),
+            "not_found"                  => __( "No Contract types found.", "estateagency"),
+            "no_terms"                   => __( "No Contract types", "estateagency"),
+            "items_list_navigation"      => __( "Contract types list navigation", "estateagency"),
+            "items_list"                 => __( "Contract types list", "estateagency"),
+            "back_to_items"              => __( "&larr; Back to Contract types", "estateagency"),
+        ],
+        "hierarchical" => true,
+        "meta_box_cb" => "post_categories_meta_box"
+    ]);
+
+
+    register_taxonomy("property_type", "property", [
+        "labels" => [
+            "name"                       => __( "Types", "estateagency"),
+            "singular_name"              => __( "Type", "estateagency"),
+            "search_items"               => __( "Search Types", "estateagency"),
+            "popular_items"              => __( "Popular Types", "estateagency"),
+            "all_items"                  => __( "All Types", "estateagency"),
+            "edit_item"                  => __( "Edit Type", "estateagency"),
+            "view_item"                  => __( "View Type", "estateagency"),
+            "update_item"                => __( "Update Type", "estateagency"),
+            "add_new_item"               => __( "Add New Type", "estateagency"), 
+            "new_item_name"              => __( "New Type Name", "estateagency"),
+            "separate_items_with_commas" => __( "Separate Types with commas", "estateagency"),
+            "add_or_remove_items"        => __( "Add or remove Types", "estateagency"),
+            "choose_from_most_used"      => __( "Choose from the most used Types", "estateagency"),
+            "not_found"                  => __( "No Types found.", "estateagency"),
+            "no_terms"                   => __( "No Types", "estateagency"),
+            "items_list_navigation"      => __( "Types list navigation", "estateagency"),
+            "items_list"                 => __( "Types list", "estateagency"),
+            "back_to_items"              => __( "&larr; Back to Types", "estateagency"),
+        ],
+        "hierarchical" => true,
+        "meta_box_cb" => "post_categories_meta_box"
+    ]);
+
+
+    register_taxonomy("property_agent", "property", [
+        "labels" => [
+            "name"                       => __( "Agents", "estateagency"),
+            "singular_name"              => __( "Agent", "estateagency"),
+            "search_items"               => __( "Search Agents", "estateagency"),
+            "popular_items"              => __( "Popular Agents", "estateagency"),
+            "all_items"                  => __( "All Agents", "estateagency"),
+            "edit_item"                  => __( "Edit Agent", "estateagency"),
+            "view_item"                  => __( "View Agent", "estateagency"),
+            "update_item"                => __( "Update Agent", "estateagency"),
+            "add_new_item"               => __( "Add New Agent", "estateagency"), 
+            "new_item_name"              => __( "New Agent Name", "estateagency"),
+            "separate_items_with_commas" => __( "Separate Agents with commas", "estateagency"),
+            "add_or_remove_items"        => __( "Add or remove Agents", "estateagency"),
+            "choose_from_most_used"      => __( "Choose from the most used Agents", "estateagency"),
+            "not_found"                  => __( "No Agents found.", "estateagency"),
+            "no_terms"                   => __( "No Agents", "estateagency"),
+            "items_list_navigation"      => __( "Agents list navigation", "estateagency"),
+            "items_list"                 => __( "Agents list", "estateagency"),
+            "back_to_items"              => __( "&larr; Back to Agents", "estateagency"),
+        ],
+        "hierarchical" => true,
+        "meta_box_cb" => "post_categories_meta_box"
+    ]);
 }
 
 
