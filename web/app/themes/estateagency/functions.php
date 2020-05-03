b757b441e3e3cc4465a7a47e82a548f36d2a8c1a
jeyofdev <jgregoire.pro@gmail.com>
2020-05-03 18:02:16 +0200

    redefine the metaboxes of taxonomies on post type 'property'

--- web/app/themes/estateagency/functions.php
+++ web/app/themes/estateagency/functions.php
@@ -13,8 +13,6 @@ require_once "class/helpers/class_estateagency_format_helpers.php";
 require_once "class/options/class_estateagency_option_agency.php";
 require_once "class/class_estateagency_title.php";
 require_once "class/walkers/class_estateagency_comment_walker.php";
-
+require_once "class/walkers/class_estateagency_contract_types_radio_walker.php";
 
 EstateAgencyOptionAgency::register();
-
-
